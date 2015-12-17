38c07bfe5797a057bcba065000117b28be886bed
Ron Kurr <kurr@kurron.org>
2015-12-17 01:34:46 +0000

    Fixed the launch script

--- ant.sh
+++ ant.sh
@@ -2,13 +2,11 @@
 
 CMD="docker run \
        --rm \
-       --name intellij \
+       --name ant \
        --net "host" \
-       --env DISPLAY=unix$DISPLAY \
        --user 1000:1000 \
-       --volume /tmp/.X11-unix:/tmp/.X11-unix \
        --volume $HOME:/home/developer \
-       kurron/docker-intellij:latest"
+       kurron/docker-ant:latest"
 
-echo $CMD
-$CMD
+#echo $CMD
+eval $CMD $*
